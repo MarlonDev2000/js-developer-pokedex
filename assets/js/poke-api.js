d9889814cba8841308bf027bafd94d847c5335c8
Marlon Braga Filho <marloncs13@outlook.com>
2024-10-20 19:56:24 -0300

    Ainda sem modal de pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,4 +1,3 @@
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -10,26 +9,29 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const [type] = types
 
     pokemon.types = types
-    pokemon.type = type
-
+    pokemon.type = type 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
 }
 
+
+// Request em pokemon url detalhada dentro do array pokemons
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+        .then((pokemon) => convertPokeApiDetailToPokemon(pokemon))
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url)
+// Request da lista de todos os pokemons onde inclui o pokemon url que usamos acima
+pokeApi.getPokemons = (offset = 0, limit = 6) => {
+    const URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(URL)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+        .then((pokemonDetails) => pokemonDetails)
 }
+
